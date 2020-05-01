7fcc25d35c2f04975764b8b3b52643d6482bbec4
Julien Mottet <mottet.julien123@gmail.com>
2020-05-01 18:38:05 +0200

    test js for airtable data

--- assets/js/importimages.js
+++ assets/js/importimages.js
@@ -1,1 +1,22 @@
-document.write(5 + 6);
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyZb12cZRDYPXpOW'}).base('apppCwdY0OP5mfZBY');
+
+base('Structures').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Logo'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
