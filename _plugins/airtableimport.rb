cf8a7d011aa8617e448a9231898cd2c9d9afa016
Julien Mottet <mottet.julien123@gmail.com>
2020-05-02 14:41:38 +0200

    add pictures

--- _plugins/airtableimport.rb
+++ _plugins/airtableimport.rb
@@ -9,8 +9,8 @@ require 'airrecord'
   end
 end
 
- File.open("_data/entreprises.json", "w") do |f|
-    data = @data.all.map { |record| record.attributes}
+ File.open("_data/entreprises2.yml", "w") do |f|
+    data = @data.all.map { |record| record["Logo"]}
     dataclean = data.reject { |e| e.to_s.empty? }
-    f.write(dataclean.to_json)
+    f.write(dataclean.to_yaml)
 end
