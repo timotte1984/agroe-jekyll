4ca979c5ae5b063444a4dad604e921fe9dbb82ce
Julien Mottet <mottet.julien123@gmail.com>
2020-05-02 14:08:52 +0200

    test'

--- _plugins/airtableimport.rb
+++ _plugins/airtableimport.rb
@@ -10,7 +10,7 @@ require 'airrecord'
 end
 
  File.open("_data/entreprises.json", "w") do |f|
-    data = @data.all.map { |record| record["Logo"]}
+    data = @data.all.map { |record| record.attributes}
     dataclean = data.reject { |e| e.to_s.empty? }
     f.write(dataclean.to_json)
 end
